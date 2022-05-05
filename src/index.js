59de668df05145054292ed86662806d5371cff60
cm val <cmvaltime@gmail.com>
2022-05-05 15:34:56 +0300

    feat: css and js files update in src

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import "./styles/style.css";
 import "./styles/style.scss";
 
-class Keyboard {
-	constructor() {
-		
-	}
-}
+const body = document.body;
+const wrapper = document.createElement('div');
+const keybord = document.querySelector('.keybord-area');
+const textArea = document.querySelector('.text-area');
+
+wrapper.classList.add('wrapper');
+body.append(wrapper);
+wrapper.insertAdjacentHTML('afterbegin', '<h1 class="task">Virtual Keyboard</h1>');
+wrapper.insertAdjacentHTML('beforeend', '<p class="description">Change language -  LeftShift + LeftCtrl. The keyboard was made in the MacOs system.</p>');
+wrapper.insertAdjacentHTML('beforeend', '<textarea autofocus placeholder="Text here..." class="text-area"  autofocus></textarea>');
+wrapper.insertAdjacentHTML('beforeend', '<div class="keybord-area"></div>');
+
+
+
